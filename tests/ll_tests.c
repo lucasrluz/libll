341847da8989cece9fe6d2e74eed1505aa3debe9
Lucas Ribeiro da Luz <lucasrluz.23@gmail.com>
2025-11-13 08:22:48 -0300

    test: adicionado testes para ll_strncmp

--- tests/ll_tests.c
+++ tests/ll_tests.c
@@ -12,4 +12,15 @@ int main(void) {
     assert(ll_strcmp("foo", "foo") == 0);
     assert(ll_strcmp("boo", "foo") == -4);
     assert(ll_strcmp("foo", "boo") == 4);
+
+    // ll_strncmp
+    assert(ll_strncmp("", "", 0) == 0);
+    assert(ll_strncmp("", "", 1) == 0);
+    assert(ll_strncmp("foo", "foo", 2) == 0);
+    assert(ll_strncmp("foo", "foo", 3) == 0);
+    assert(ll_strncmp("boo", "foo", 1) == -4);
+    assert(ll_strncmp("foo", "boo", 1) == 4);
+    assert(ll_strncmp("boo", "foo", 3) == -4);
+    assert(ll_strncmp("foo", "boo", 3) == 4);
+    
 }
